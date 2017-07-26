11d8610adc55c78c17f36f409973967bf0bbbc29
son0p <fede2001@gmail.com>
2017-07-26 14:35:53 -0500

    cambia rango de fechas, cantidad, y agrega global.r

--- ui.R
+++ ui.R
@@ -10,8 +10,8 @@ ui <- function(request) {
     leafletOutput("map", width = "100%", height = "100%"),
     absolutePanel(top = 10, right = 10,
                   titlePanel("Apariciones de grupos "),
-                  sliderInput("range", "Capacidad", 0, 50000,value = c(10,3000), step = 1 ),
-                  dateRangeInput("dates", start = "2010-06-01", end = "2018-01-01", label = h3("Rango de fechas")),
+                  sliderInput("range", "Capacidad", 0, 20000,value = c(10,3000), step = 1 ),
+                  dateRangeInput("dates", start = "1970-01-01", end = "2018-01-01", label = h3("Rango de fechas")),
                   selectInput("wday", "Día de la semana", c("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"), multiple=TRUE, selectize=TRUE, selected = c("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday")),
                   bookmarkButton()
                   )
